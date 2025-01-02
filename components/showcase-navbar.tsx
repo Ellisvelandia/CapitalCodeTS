b9815af20f6c542d6d0dd418fcd5c68cbfd979d8
Ellis Velandia <eyis619@gmail.com>
2025-01-02 15:46:37 -0500

    image projects

--- components/showcase-navbar.tsx
+++ components/showcase-navbar.tsx
@@ -32,7 +32,7 @@ xl:w-4/5
     >
       <Link href="/">
         <Image
-          src="/logo/logo.svg"
+          src="/logo/logo.webp"
           alt="Capital Code Logo"
           width={1000}
           height={1000}
