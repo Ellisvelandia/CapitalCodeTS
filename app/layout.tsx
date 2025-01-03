1ed763b01d883b8b88d374731bef9083c835523b
Ellis Velandia <eyis619@gmail.com>
2025-01-02 20:04:06 -0500

    meta logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,14 +11,42 @@ export const metadata: Metadata = {
   title: "Capital Code | Desarrollo de Software y Sitios Web Personalizados",
   description: "Expertos en desarrollo de software y sitios web personalizados para empresas e individuos. Transformamos ideas en soluciones tecnológicas innovadoras.",
   openGraph: {
+    title: "Capital Code | Desarrollo de Software",
+    description: "Expertos en desarrollo de software y sitios web personalizados",
+    url: 'https://capitalcode.com.ec',
+    siteName: 'Capital Code',
+    locale: 'es_EC',
+    type: 'website',
     images: [
       {
-        url: 'https://www.google.com/maps/uv?viewerState=lb&pb=!1s0x11ae81c94242f451:0x2a6b0cac40414137!5sGlobstand+technologies&imagekey=!1e10!2sAF1QipPrTTNGxsOszQTXFD3b68e40o0V_LiVsTWwh-H6&cr=rp_35',
+        url: '/logo/logo.webp',
         width: 1200,
         height: 630,
         alt: 'Capital Code - Desarrollo de Software',
       },
     ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Capital Code | Desarrollo de Software',
+    description: 'Expertos en desarrollo de software y sitios web personalizados',
+    images: ['/logo/logo.webp'],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
   },
 };
 
