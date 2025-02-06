284b5d6acae3f903a837c4053f4da5e3af29b9c0
ellis  velandia caicedo <100318892+Ellisvelandia@users.noreply.github.com>
2025-02-06 13:45:48 -0500

    Add configuration file with app details and domain name

---
+++ config.ts
@@ -0,0 +1,8 @@
+const config = {
+    domainName: "https://capital-code.vercel.app/",
+    appName: "Capital Code",
+    appDescription:
+      "Capital Code is a modern web platform for Capital Code, showcasing our software development and web solutions services.",
+  };
+  
+  export default config;
